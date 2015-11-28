6506c8ebb9f9af056762d4b93625bcd2ab2a6b4d
Cristof17 <cristof_rotsching@yahoo.com>
2015-11-28 18:05:14 +0200

    parsing words

--- src/com/cristof/MapReduce/Map/MapWorker.java
+++ src/com/cristof/MapReduce/Map/MapWorker.java
@@ -1,9 +1,14 @@
 package com.cristof.MapReduce.Map;
 
+import java.beans.DesignMode;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -27,93 +32,52 @@ public class MapWorker extends Thread {
 	 * 	Daca s-a ajuns la o solutie finala, aceasta va fi afisata.
 	 */
 	public void processPartialText(PartialText ps) {
-
-//		 if(wp.getWork() == null){
-//		 	return;
-//		 	
-		if(2 > 4){
-		
-		
-		}else{
-			//TODO 
-			//de facut ce trebuie sa faca un worker
 			
 			int numberOfChars =(int) (ps.stop - ps.start + 1); 
-			char[] destination_buffer = new char[numberOfChars];
-			
-			
+			byte[] destination_buffer =  new byte[10000 ];
+						
 			try {
 				
-				File sourceFile = new File(ps.fileName);		
-				BufferedReader fileBufferedReader = new BufferedReader(new FileReader(sourceFile));
-				fileBufferedReader.mark((int) ps.start);
-				fileBufferedReader.read(destination_buffer, (int)ps.start , numberOfChars);
-				
-				
-				//DEBUG
-				System.out.println("------INITIAL------");
-				System.out.println(destination_buffer);
-				System.out.println("------END INITIAL------");
-				
-			
-				String fragmentText = new String(destination_buffer);
-				StringBuilder fragmentBuider = new StringBuilder();
-				
-				
-				StringTokenizer st = new StringTokenizer(fragmentText, delimitators); 
-				
-				//check the beginning of the word 
-				if(! delimitators.contains(new Character(destination_buffer[0]).toString()) && ps.start > 0){
-					//jump over the first word 
-					//eliminate the first word
-					st.nextElement();
-					
-				}
-								
-				
-				if(! delimitators.contains(new Character(destination_buffer[destination_buffer.length -1]).toString())){
-					//process the last word as well
-					int offset = 0;
-					char outputFromOffset[] = {'A'}; //stores the next Byte 
+				RandomAccessFile raf = new  RandomAccessFile(new File(ps.fileName),"rw");
+				int charRead = 0;
+				StringBuilder word = new StringBuilder();
+				raf.seek(ps.start);
+				while(charRead <= numberOfChars){
 					while(true){
-						//read one character, store in the one char buffer and then check if it is a delimiter
-						Character nextChar = new Character(outputFromOffset[0]);
-						fileBufferedReader.mark(destination_buffer.length + offset - 1);
-						fileBufferedReader.read(outputFromOffset);
-						if(!delimitators.contains(nextChar.toString())){
-							//still in the middle of the word
-							//concatenate current byte to the fragment buffer
-							StringBuilder sb = new StringBuilder();
-							sb.append(destination_buffer);
-							sb.append(outputFromOffset);
-							destination_buffer = sb.toString().toCharArray();
-							offset++;
-						}else{
-							//it is a delimiter
+						byte caracter = raf.readByte();
+						if(delimitators.contains(new Character((char)caracter).toString())){
+							charRead = 0;
 							break;
+						}else{
+							word.append((char)caracter);
+							charRead ++;
 						}
 					}
 					
+					if(word.toString()!= ""){
+						System.out.println(word.toString());
+					}
+					
+					processWord(word.toString());
+					
+					word = new StringBuilder();
+					
 				}
-				//start processing from the second string
-		
-				
-				//DEBUG
-				System.out.println("------AFTER LIMITS------");
-				System.out.println(destination_buffer);
-				System.out.println("------END LIMITS------");
-				
-				fileBufferedReader.close();
 				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+//				e.printStackTrace();
 			}
 			
 			
+			
 		 }
-	}
 	
+	private void processWord(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public void run() {
 		System.out.println("Thread-ul worker " + this.getName() + " a pornit...");
 		PartialText ps ;
